bfef7adcb56f817a1e895ff76aeee351d078d205
YatharthDedhia <dedhiayatharth2004@gmail.com>
2022-03-28 19:37:00 +0530

    Added comments

--- Fibonacci Blinker.c
+++ Fibonacci Blinker.c
@@ -1,3 +1,15 @@
+       /*  BLINKIN LEDS IN FIBONACCI SEQUENCE
+
+        EXPECTED OUTPUT:
+        0 = 00000000 = 0x00
+        1 = 00000001 = 0x01
+        1 = 00000001 = 0x01
+        2 = 00000011 = 0x03
+        3 = 00000111 = 0x07
+        5 = 00011111 = 0x1F
+        8 = 11111111 = 0xFF
+    */
+    // Header Files
     #include <stdio.h>
     #include "freertos/FreeRTOS.h"
     #include "freertos/task.h"
@@ -11,50 +23,88 @@
         
         while(1)
         {
-            
-            int term_1,term_2,term_3;
+            //Initializing variables for Fibonacci numbers
+            int fib_no,t2,t3;
             int i = 0;
-            term_1 = 0;
-            term_2  = 1;
+            fib_no = 0;
+            t2  = 1;
 
-            while(i<7)
+            //Fibonacci loop
+            while(i<7) //7 because outputting for 8 LEDS
             {
-                uint8_t a0 = 0x00;
-                uint8_t a1 = 0x01;
-                uint8_t a2 = 0x01;
+                //Initializing variables for LED Blink
+                uint8_t LED_OFF = 0x00;
+                uint8_t LED_1 = 0x01;
+                uint8_t LED_ON = 0x01;
 
-                if(term_1 ==0 )
+                //IF and ELSE IF Ladder For 1st two OUTPUTS
+                if(fib_no ==0 )
                 {
-                    ESP_ERROR_CHECK(set_bar_graph(a0));
+                    //Display 00000000
+                    ESP_ERROR_CHECK(set_bar_graph(LED_OFF));
                     vTaskDelay(1000 / portTICK_RATE_MS);
                 }
 
-                else if(term_1 ==1)
+                else if(fib_no ==1)
                 {
-                    ESP_ERROR_CHECK(set_bar_graph(a1));
+                    //Display 00000001
+                    ESP_ERROR_CHECK(set_bar_graph(LED_1));
                     vTaskDelay(1000 / portTICK_RATE_MS);
 
-                    ESP_ERROR_CHECK(set_bar_graph(a0));
+                    //Display 00000000
+                    ESP_ERROR_CHECK(set_bar_graph(LED_OFF));
                     vTaskDelay(1000 / portTICK_RATE_MS);
                 }
-            
+                
+                //ELSE for any other value of fib_no
                 else
                 {
-                    for(int j=1; j<=term_1-1;j++)
+                    //FOR loop to get the LED blinking
+                    for(int j=1; j<=fib_no-1;j++)
                     {
-                        a2 = a2 | a1<<(term_1-j);
+                        LED_ON = LED_ON | LED_1<<(fib_no-j);
+
+                      //Loop functioning
+                      //fib_no = 2
+                      //LED_ON = 0x01 | 0x01<<1 = 00000001|00000010 = 00000011 =0x03
+            
+                      //LED_ON = 0x01
+                      //fib_no = 3
+                      //LED_ON = 0x01 | 0x01<<1 = 00000001|00000010 = 00000011 =0x03
+                      //LED_ON = 0x03 | 0x01<<2 = 00000011|00000100 = 00000111 =0x07
+
+                      //LED_ON = 0x01
+                      //fib_no = 5
+                      //LED_ON = 0x01 | 0x01<<1 = 00000001|00000010 = 00000011 =0x03
+                      //LED_ON = 0x03 | 0x01<<2 = 00000011|00000100 = 00000111 =0x07
+                      //LED_ON = 0x07 | 0x01<<3 = 00000111|00001000 = 00001111 =0x0F
+                      //LED_ON = 0x0F | 0x01<<4 = 00001111|00010000 = 00011111 =0x1F
+
+                      //LED_ON = 0x01
+                      //fib_no = 8
+                      //LED_ON = 0x01 | 0x01<<1 = 00000001|00000010 = 00000011 =0x03
+                      //LED_ON = 0x03 | 0x01<<2 = 00000011|00000100 = 00000111 =0x07
+                      //LED_ON = 0x07 | 0x01<<3 = 00000111|00001000 = 00001111 =0x0F
+                      //LED_ON = 0x0F | 0x01<<4 = 00001111|00010000 = 00011111 =0x1F
+                      //LED_ON = 0x1F | 0x01<<5 = 00011111|00100000 = 00111111 =0x3F
+                      //LED_ON = 0x03 | 0x01<<6 = 00000011|01000000 = 01111111 =0x7F
+                      //LED_ON = 0x07 | 0x01<<7 = 00000111|10000000 = 11111111 =0xFF
                     }
 
-                    ESP_ERROR_CHECK(set_bar_graph(a2));
+                    //Display LED_ON (No of LEDs to turn ON)
+                    ESP_ERROR_CHECK(set_bar_graph(LED_ON));
                     vTaskDelay(1000 / portTICK_RATE_MS);
 
-                    ESP_ERROR_CHECK(set_bar_graph(a0));
+                    ESP_ERROR_CHECK(set_bar_graph(LED_OFF));
                     vTaskDelay(1000 / portTICK_RATE_MS);
                 }
-                a2 = 0x01;
-                term_3 = term_1+term_2;
-                term_1 = term_2;
-                term_2 = term_3;
+                //Resetting LED_ON to 0x00 (00000001)
+                LED_ON = 0x01;
+
+                //To get the next fibonacci number (fib_no)
+                t3 = fib_no+t2;
+                fib_no = t2;
+                t2 = t3;
                 i++;
             }
         }  
